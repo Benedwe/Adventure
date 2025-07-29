20bc6470d6a4756cb40c824f786d03777192ac73
Benedwe <benjaedward23@gmail.com>
2025-07-29 11:51:00 +0300

    Add new files

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "math-adventure-cache-v1";
+const CACHE_NAME = "math-adventure-cache-v1.1";
 const urlsToCache = [
   "/",
   "/index.html",
@@ -9,14 +9,23 @@ const urlsToCache = [
   "/Mathblast.html",
   "/favicon.ico",
   "/service-worker.js",
-  "/Music.mp3",
+  "/music.mp3",
   "/twitter.png"
-  
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
   );
 });
 
